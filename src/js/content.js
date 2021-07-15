e17858416991f89d459bcf68ad21b2c329c4ef5a
davidcastel <davidccbp20@gmail.com>
2021-07-15 19:09:46 -0400

    Create box  for when the button is pressed

--- src/js/content.js
+++ src/js/content.js
@@ -1,4 +1,27 @@
 chrome.runtime.onMessage.addListener((request) => {
-    console.log(`Domain for the current Tab: ${request.url}`);
-    alert(`Domain for the current Tab: ${request.url}`);
+  let user_option = request.form_data;
+  console.log(user_option);
+  console.log(request.walkthrough);
+
+  const box = document.createElement("section");
+  box.id = "main-walkthrough-box";
+  box.style.backgroundColor = "white";
+  box.style.border = "black 2px solid";
+  box.style.position = "absolute";
+  box.style.top = "0px";
+  box.style.padding = "2%";
+
+  const text = `
+    <h2>Title of walthrough</h2>
+    <p>The text of this walkthough<p>
+    <div style="display: flex; justify-content: space-between">
+      <button style="border: black 2px solid; border-radius: 25px; with: 100%" onclick=""> back </button>
+      <button style="border: black 2px solid; border-radius: 25px; with: 100%" onclick=""> forward </button>
+    </div>
+  `;
+
+  box.innerHTML = text;
+
+  document.querySelector("body").appendChild(box);
+  console.log(box);
 });
